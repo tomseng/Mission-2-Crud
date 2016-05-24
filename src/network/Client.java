224986f96d86eb55b945d6e16b79a8d8141a30ca
tomseng <engelsthomas92@gmail.com>
2016-05-24 17:52:32 +0200

    jframe de merde

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,15 +1,34 @@
 package network;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Client {
+
+public class Client implements ActionListener{
 	
 	
 	public static void main(String[] args) {
 		
+		JFrame jf1 = new JFrame();
+		JPanel jp1 = new JPanel();
+		jp1.setName("panel1");
+		jf1.setSize(600, 350);
+		jf1.setTitle("GUI Client");
+		jf1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton jb1 = new JButton("Inscrire un WS");
+		jb1.setSize(40,40);
+		//jf1.add(jb1);
+		jf1.getContentPane().add(jb1);
+		jf1.setVisible(true);
+		jf1.setLocation(400, 200);
+
 		Socket socket;
 
 		try {
@@ -26,6 +45,12 @@ public class Client {
 			
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
