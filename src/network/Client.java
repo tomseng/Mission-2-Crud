012cdfb52fd2dd737683c4996d55ffd479916c57
tomseng <engelsthomas92@gmail.com>
2016-05-25 14:00:29 +0200

    gitignore

--- src/network/Client.java
+++ src/network/Client.java
@@ -26,7 +26,7 @@ public class Client implements ActionListener{
 		
 		
 		Container ct1 = new Container();
-		ArrayList<String[]> tab =  ct1.webService("read", "table1", new String[1]);
+		ArrayList<String[]> tab =  ct1.webService("read", "table1", new String[0]);
 		for(int i=0; i<tab.size(); i++){
 			for(int j=0; j<tab.get(i).length; j++){
 				System.out.print(tab.get(i)[j]+" ");
@@ -72,7 +72,7 @@ public class Client implements ActionListener{
 
 		try {
 		     //socket = new Socket(InetAddress.getLocalHost(),4485);	
-		     socket = new Socket("192.168.0.85",4485);	
+		     socket = new Socket("192.168.0.74",4485);	
 
 	             socket.close();
 
