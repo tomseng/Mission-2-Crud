8fb294658e259fc688320a53d81b46ed91bf8d90
Kevin Martin <kevin.martin@quatdev.fr>
2016-05-26 11:55:47 +0200

    client lol

--- src/network/Client.java
+++ src/network/Client.java
@@ -95,7 +95,7 @@ public class Client implements ActionListener{
 
 		try {
 		     //socket = new Socket(InetAddress.getLocalHost(),4485);	
-		     socket = new Socket("192.168.0.54",4485);
+		     socket = new Socket("192.168.0.85",4490);
 		     //socket.connect(endpoint);
 		     //BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		     ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
