9222f75161d33cb0f1d4d846f8114abfd013d889
tomseng <engelsthomas92@gmail.com>
2016-05-26 11:55:03 +0200

    Le bon registre

--- src/network/Registry.java
+++ src/network/Registry.java
@@ -26,6 +26,7 @@ public class Registry {
 	}
 	
 	public static Object[][] getEnregistrementsToObjects() {
+	
 		Object[][] retour=new Object[getEnregistrements().size()][];
 		System.out.println(enregistrements.size());
 		int i=0;
@@ -65,32 +66,50 @@ public class Registry {
 	}
 
 	
-	public static void main(String[] zero) throws SQLException, InterruptedException {
+	public static void main(String[] zero) throws SQLException, InterruptedException, ClassNotFoundException {
 		
-		ServerSocket socketserver  ;
+		ServerSocket socketServerContainer  ;
 		Socket socketduserveur ;
 				
 		//TestApp();
 		try {
 			//ct1.main(args);
-			socketserver = new ServerSocket(4485);
-			//Container ct1=new Container(socketserver.getInetAddress().toString(), "4485");
-			System.out.println("Le registre écoute les connexions sur le port "  + socketserver.getLocalPort()+"...");
+			socketServerContainer = new ServerSocket(4485);
 
-			socketduserveur = socketserver.accept(); 
+			//Container ct1=new Container(socketServerContainer.getInetAddress().toString(), "4485");
+			System.out.println("Le registre écoute les connexions sur le port "  + socketServerContainer.getLocalPort()+"...");
+
+			socketduserveur = socketServerContainer.accept();
+			ObjectInputStream ois = new ObjectInputStream(socketduserveur.getInputStream());
+			Object[][] donne = (Object[][]) ois.readObject();
+	    	System.out.println("recue du container     << "+donne[0][0]);
+			
+	    	/**ObjectOutputStream fluxEcriture = new ObjectOutputStream(socketduserveur.getOutputStream());
+			//PrintWriter pw=new PrintWriter(socketduserveur.getOutputStream());
+			System.out.println("Un client s'est connecté !");
+			//for (int i=0; i<10; i++)
+			//{
+			//	System.out.println(getEnregistrementsToObjects()[0][2]);
+				fluxEcriture.writeObject(donne);
+				//pw.println("dsvffbf");
+				Thread.currentThread().sleep(2000);*/
+				//fluxEcriture.close();
+			//}
+			
+		       socketServerContainer.close();
+			ServerSocket socketServeurClient = new ServerSocket(4490);
+			System.out.println("Le registre écoute les connexions sur le port "  + socketServeurClient.getLocalPort()+"...");
+
+			socketduserveur = socketServeurClient.accept();
 			ObjectOutputStream fluxEcriture = new ObjectOutputStream(socketduserveur.getOutputStream());
 			//PrintWriter pw=new PrintWriter(socketduserveur.getOutputStream());
 			System.out.println("Un client s'est connecté !");
 			//for (int i=0; i<10; i++)
 			//{
-				System.out.println(getEnregistrementsToObjects()[0][2]);
-				fluxEcriture.writeObject(getEnregistrementsToObjects());
+			//	System.out.println(getEnregistrementsToObjects()[0][2]);
+				fluxEcriture.writeObject(donne);
 				//pw.println("dsvffbf");
 				Thread.currentThread().sleep(2000);
-				//fluxEcriture.close();
-			//}
-			
-		       socketserver.close();
 		       socketduserveur.close();
 
 		}catch (IOException e) {
